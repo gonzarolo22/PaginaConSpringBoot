8753dc8caee0b8bcef6b74790c01e19212285262
gonza rolo <gonzarolo22@gmail.com>
2022-12-28 02:13:45 -0300

    implementacion de rest

--- src/main/java/com/example/Proyecto/controller/UserRestController.java
+++ src/main/java/com/example/Proyecto/controller/UserRestController.java
@@ -1,7 +1,24 @@
 package com.example.Proyecto.controller;
 
+import com.example.Proyecto.caseUse.getUser;
+import com.example.Proyecto.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/users")
 public class UserRestController {
+    private getUser getuser;
+
+    public UserRestController(getUser getuser) {
+        this.getuser = getuser;
+    }
+
+    @GetMapping("/")
+    List<User>get(){
+        return getuser.getAll();
+    }
 }
